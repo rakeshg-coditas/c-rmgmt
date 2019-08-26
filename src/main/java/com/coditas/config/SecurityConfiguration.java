6f22d86739fc8d260813948be034d81343e2cf5f
rakeshg-coditas <rakesh.ghonmode@coditas.com>
2019-08-26 13:27:21 +0530

    authenticated apis

--- src/main/java/com/coditas/config/SecurityConfiguration.java
+++ src/main/java/com/coditas/config/SecurityConfiguration.java
@@ -81,7 +81,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/authenticate").permitAll()
             .antMatchers("/api/register").permitAll()
             .antMatchers("/api/activate").permitAll()
-            .antMatchers("/api/user/login").permitAll()
             .antMatchers("/api/account/reset-password/init").permitAll()
             .antMatchers("/api/account/reset-password/finish").permitAll()
             .antMatchers("/api/**").authenticated()
